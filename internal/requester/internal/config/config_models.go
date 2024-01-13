05139499501e0a1298cf137dde2b0885876b9aae
Lisandro Torresetti <ltorresetti@fi.uba.ar>
2024-01-13 11:17:14 -0300

    + Refactor config
    + Add tests for config_models

--- internal/requester/internal/config/config_models.go
+++ internal/requester/internal/config/config_models.go
@@ -1,8 +1,8 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
-	"gopkg.in/yaml.v3"
 )
 
 const defaultLimit = 10
@@ -12,10 +12,55 @@ type ServiceEndpoints struct {
 	Endpoints map[string]Endpoint `json:"endpoints"`
 }
 
+func (se *ServiceEndpoints) UnmarshalJSON(rawServiceData []byte) error {
+	if len(rawServiceData) == 0 {
+		return errServiceEndpointDataMissing
+	}
+
+	var serviceEndpoints struct {
+		Base      string              `json:"base"`
+		Endpoints map[string]Endpoint `json:"endpoints"`
+	}
+
+	err := json.Unmarshal(rawServiceData, &serviceEndpoints)
+	if err != nil {
+		return errUnmarshallingServiceData
+	}
+
+	se.Base = serviceEndpoints.Base
+	se.Endpoints = serviceEndpoints.Endpoints
+
+	for key, endpointData := range se.Endpoints {
+		endpointData.setBaseURL(se.Base)
+		se.Endpoints[key] = endpointData
+	}
+
+	return nil
+}
+
+// GetEndpoint returns the service endpoint based on the given alias
+func (se *ServiceEndpoints) GetEndpoint(endpointAlias string) (Endpoint, error) {
+	endpointData, endpointExists := se.Endpoints[endpointAlias]
+	if !endpointExists {
+		return Endpoint{}, fmt.Errorf("%w: %s", errEndpointDoesNotExist, endpointAlias)
+	}
+
+	return endpointData, nil
+}
+
 type Endpoint struct {
 	Path        string       `json:"path"`
 	Method      string       `json:"method"`
 	QueryParams *QueryParams `json:"query_params"`
+	baseURL     string
+}
+
+func (e *Endpoint) setBaseURL(base string) {
+	e.baseURL = base
+}
+
+func (e *Endpoint) GetURL() string {
+	return e.baseURL + e.Path
 }
 
 type QueryParams struct {
@@ -23,32 +68,34 @@ type QueryParams struct {
 	Limit  int `json:"limit"`
 }
 
-func (qp *QueryParams) UnmarshalYAML(value *yaml.Node) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-/*func (qp *QueryParams) UnmarshalJSON(rawData []byte) error {
+func (qp *QueryParams) UnmarshalJSON(rawData []byte) error {
 	if len(rawData) == 0 {
 		return nil
 	}
 
-	qp.Limit = 100
-	qp.Offset = 0
+	var queryParams struct {
+		Offset int `json:"offset"`
+		Limit  int `json:"limit"`
+	}
+	err := json.Unmarshal(rawData, &queryParams)
+	if err != nil {
+		return fmt.Errorf("%w: %w", errUnmarshallingQueryParams, err)
+	}
+
+	qp.Limit = queryParams.Limit
+	if qp.Limit == 0 {
+		qp.Limit = defaultLimit
+	}
+	qp.Offset = queryParams.Offset
 
 	return nil
-}*/
+}
 
+// ToMap converts QueryParams struct into a map
 func (qp *QueryParams) ToMap() map[string]string {
 	paramsMap := make(map[string]string)
 
-	limit := qp.Limit
-	// ToDo: add this logic in UnmarshalJSON
-	if limit == 0 {
-		limit = defaultLimit
-	}
-
-	paramsMap["limit"] = fmt.Sprintf("%v", limit)
+	paramsMap["limit"] = fmt.Sprintf("%v", qp.Limit)
 	paramsMap["offset"] = fmt.Sprintf("%v", qp.Offset)
 
 	return paramsMap
